c72b3f920acb101df22dcede90b8bf3013f06050
denisshragin <49815046+denisshragin@users.noreply.github.com>
2022-01-09 07:04:16 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Google spreadsheets configuration
-gs_json_config = "covid-qc-2021-38322308cd34.json"
-gs_key_cas = "1gDEMNLJ4TokOrzKRJZpQ6853Y71fq5p298iQhdseXmc" #number of a new COVID cases in Quebec province.
-gs_key_cas_total = "1xNRjaGQq9OJo4V4B9ElYH8r4XZkUUHWlYs1xRdadks0"#Total number of a new COVID cases in Quebec province.
-gs_key_deaths = "1slbT7jHrE6LhLE_MuARkxD9iM4KOMWJfeAySI5q8HnM"
-gs_key_investigations = "1dpK00S7Mfhn6OR92rWI3bwBolsaFBveVx5IJM4bUujk"
-gs_key_vaccin = "1Kbrt5NllO8zidsrNZPY2YZNtYl4IglwyAxppKlsQZcM"
+gs_json_config = 
+gs_key_cas =  #number of a new COVID cases in Quebec province.
+gs_key_cas_total =  #Total number of a new COVID cases in Quebec province.
+gs_key_deaths = 
+gs_key_investigations = 
+gs_key_vaccin = 
